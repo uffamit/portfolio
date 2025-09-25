49e1daa0de559902e8db69e4233ee40451b7c66a
Amit Divekar <amitdivekar289@gmail.com>
2025-09-25 15:23:45 +0000

    change that favicon.svg in every page some pages change that like if go

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
