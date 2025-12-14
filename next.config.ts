1aa372caf9f80a86b76e44251c1980713281b64f
Amit Divekar <amitdivekar289@gmail.com>
2025-12-14 08:14:24 +0000

    The Problem Looking at your certificate section in [src/data/portfolio-d

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'logo.clearbit.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
